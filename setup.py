72612a90859a6c459220a858c0587314f21dc4a8
So Negishi <5055ngs@gmail.com>
2021-11-25 07:38:17 +0900

    Update setup

--- setup.py
+++ setup.py
@@ -3,37 +3,30 @@ from pathlib import Path
 import versioneer
 
 
-here = Path(__file__).parent
+_CURRENT_DIRECTORY: str = Path(__file__).parent
 
-with (here / 'README.md').open('r') as f:
-    long_description = '\n' + f.read()
+with (_CURRENT_DIRECTORY / "README.md").open("r") as f:
+    long_description: str = "\n" + f.read()
 
-with open('requirements.txt') as f:
-    lines = f.read().split('\n')
-    install_requires = [line.split()[0] for line in lines if line]
 
 setup(
-    name='jinjalint',
-    author='Antoine Motet',
-    author_email='antoine.motet@gmail.com',
-    url='https://github.com/motet-a/jinjalint',
+    name="jinjalint",
+    author="JDSC, Co., Ltd.",
+    url="https://github.com/jdsc/jinjalint",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='A linter for Jinja-like templates',
+    description="A linter for Jinja-like templates",
     long_description=long_description,
-    packages=['jinjalint'],
+    packages=["jinjalint"],
     include_package_data=True,
-    license='MIT',
-    install_requires=install_requires,
+    license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
     ],
     entry_points={
-        'console_scripts': ['jinjalint=jinjalint.cli:main'],
+        "console_scripts": ["jinjalint=jinjalint.cli:main"],
     },
 )
