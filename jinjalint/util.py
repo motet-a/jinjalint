1455de1daac60ee4846f9d1abf5ee10f4af36e18
Pascal Corpet <pascal@co2ai.com>
2023-06-01 23:50:13 +0200

    Add Python 3.10 and 3.11 support.

--- jinjalint/util.py
+++ jinjalint/util.py
@@ -1,4 +1,4 @@
-import collections
+from collections.abc import Iterable
 
 
 def flatten(l):
@@ -6,7 +6,7 @@ def flatten(l):
     Deeply flattens an iterable.
     """
     for el in l:
-        if (isinstance(el, collections.Iterable) and
+        if (isinstance(el, Iterable) and
                 not isinstance(el, (str, bytes))):
             yield from flatten(el)
         else:
